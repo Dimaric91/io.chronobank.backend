3a564bd434d5f67bb801d4bf77149a9285393b0e
dvvilegzhanin <dvvylegzhanin@chel.rucard.ru>
2017-11-10 16:35:27 +0300

    delete stub

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,6 @@ const restful = require('restful-keystone')(keystone, {
 const SignInService = require('./signin/SignInService')
 const UserService = require('./users/UserService')
 
-const Product = keystone.list('Product')
-const Enquiry = keystone.list('Enquiry')
-const Application = keystone.list('Application')
-const Subscription = keystone.list('Subscription')
-const Header = keystone.list('Header')
-const Story = keystone.list('Story')
-const FaqTopic = keystone.list('FaqTopic')
-const FaqQuestion = keystone.list('FaqQuestion')
-
-const FaqQuestionIndex = require('../index/FaqQuestionIndex')
-
 // Common Middleware
 keystone.pre('routes', middleware.initLocals)
 keystone.pre('render', middleware.flashMessages)
@@ -46,91 +35,6 @@ exports = module.exports = function (app) {
   app.get('/api/v1/swagger.json', function (req, res) {
     res.setHeader('Content-Type', 'application/json')
     res.send(keystone.get('swaggerSpec'))
-  })
-
-  app.get('/api/v1/products/s/:slug', async (req, res) => {
-    const product = await Product.model
-      .findOne({
-        'slug': req.params.slug
-      })
-      .populate('downloads')
-      .populate('distros')
-      .populate('features')
-      .exec()
-    res.send(product)
-  })
-
-  app.get('/api/v1/headers/s/:slug', async (req, res) => {
-    const product = await Header.model
-      .findOne({
-        'slug': req.params.slug
-      })
-      .exec()
-    res.send(product)
-  })
-
-  app.get('/api/v1/stories/s/:slug', async (req, res) => {
-    const product = await Story.model
-      .findOne({
-        'slug': req.params.slug
-      })
-      .exec()
-    res.send(product)
-  })
-
-  // eslint-disable-next-line
-  app.get('/api/v1/faq-questions/search', async (req, res) => {
-    const page = await FaqQuestion.model.search(req.query)
-    res.send(page)
-  })
-
-  app.get('/api/v1/faq-questions/reindex', async (req, res) => {
-    await FaqQuestionIndex.clearIndex()
-    await FaqQuestionIndex.saveIndex(await FaqQuestion.model.find().exec())
-    res.send('OK')
-  })
-
-  app.post('/api/v1/enquiries', async (req, res) => {
-    const body = req.body
-    const persisted = await Enquiry.model.create({
-      name: body.name,
-      email: body.email,
-      phone: body.phone,
-      enquiryType: 'message',
-      message: body.message
-    })
-    res.send(persisted)
-  })
-
-  app.post('/api/v1/subscriptions', async (req, res) => {
-    const body = req.body
-    const persisted = await Subscription.model.create({
-      email: body.email
-    })
-    res.send(persisted)
-  })
-
-  app.post('/api/v1/applications', async (req, res) => {
-    const body = req.body
-    const persisted = await Application.model.create({
-      name: body.name,
-      email: body.email,
-      phone: body.phone,
-      job: body.job,
-      message: body.message
-    })
-    res.send(persisted)
-  })
-
-  app.get('/api/v1/faq-topics', async (req, res) => {
-    const topics = await FaqTopic.model
-      .find()
-      .populate('questions')
-      .exec()
-    res.send(topics.map(topic => ({
-      ...topic.toJSON(),
-      questions: [...topic.questions]
-    })))
   })
 
   /**
@@ -238,46 +142,5 @@ exports = module.exports = function (app) {
   app.post('/api/v1/signin', async (req, res) => {
     return SignInService.checkUserAccess(req, res)
   })
-
-  restful.expose({
-    Article: {
-      methods: ['list', 'retrieve']
-    },
-    Feature: {
-      methods: ['list', 'retrieve']
-    },
-    Job: {
-      methods: ['list', 'retrieve']
-    },
-    Header: {
-      methods: ['list', 'retrieve'],
-    },
-    Iteration: {
-      methods: ['list', 'retrieve'],
-    },
-    Member: {
-      methods: ['list', 'retrieve']
-    },
-    Partner: {
-      methods: ['list', 'retrieve']
-    },
-    Post: {
-      methods: ['list', 'retrieve'],
-      populate: ['categories', 'author'],
-    },
-    Product: {
-      methods: ['list', 'retrieve'],
-      populate: ['downloads', 'distros', 'features'],
-    },
-    Statistic: {
-      methods: ['list', 'retrieve']
-    },
-    Story: {
-      methods: ['list', 'retrieve'],
-    },
-    Testimonial: {
-      methods: ['list', 'retrieve'],
-    }
-  })
-    .start()
+  
 }
